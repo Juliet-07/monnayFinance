0cf5cbafc42d5e49d4febd3d4ff241504a8b4956
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2022-07-30 19:41:54 +0100

    terms

--- pages/terms.js
+++ pages/terms.js
@@ -31,16 +31,16 @@ const Terms = () => {
             <h5 className={styles.termsListHeader}>Legal Issues</h5>
             <ul className={styles.termsList}>
               <li style={{ margin: "15px" }}>
-                Terms & Conditions of Service as set forth herein shall govern
-                the relationship between the Participant and the Fund and are
-                effective immediately after the Participant has made a deposit
-                with the online investment program provided by the Fund and
-                shall be valid till the deposit term expires.
+                Terms {`&`} Conditions of Service as set forth herein shall
+                govern the relationship between the Participant and the Fund and
+                are effective immediately after the Participant has made a
+                deposit with the online investment program provided by the Fund
+                and shall be valid till the deposit term expires.
               </li>
               <li style={{ margin: "15px" }}>
                 The Participant must be of legal age in his country of residence
                 to be able to create an account with the Monnay Finance online
-                investment program, and in all the cases the Participant's
+                investment program, and in all the cases the {`Participant's`}
                 minimal age must be 18 years.
               </li>
               <li style={{ margin: "15px" }}>
@@ -53,15 +53,17 @@ const Terms = () => {
                 taxes and related matters.
               </li>
               <li style={{ margin: "15px" }}>
-                The Firm does not offer advices on the suitability of the Firm's
-                services for the Participant's individual needs, nor does the
-                Firm provides for any judgment on the Participant's behalf,
-                relating to government or tax regulations in particular. It is
-                the responsibility of the Investor to take independent
+                The Firm does not offer advices on the suitability of the{" "}
+                {`Firm's`}
+                services for the {`Participant's`} individual needs, nor does
+                the Firm provides for any judgment on the {`Participant's`}{" "}
+                behalf, relating to government or tax regulations in particular.
+                It is the responsibility of the Investor to take independent
                 assessment as to the suitability of said investment scheme for
-                the Participant's particular circumstances. No information on
-                the Firm's website is to be considered as a solicitation or an
-                offer to participate in investment schemes the Firm provides.
+                the {`Participant's`} particular circumstances. No information
+                on the {`Firm's`} website is to be considered as a solicitation
+                or an offer to participate in investment schemes the Firm
+                provides.
               </li>
               <li style={{ margin: "15px" }}>
                 Every deposit is considered to be a private transaction between
@@ -95,7 +97,7 @@ const Terms = () => {
                 The Participant is provided with a fixed daily interest or
                 return at the rate that depends on the amount of deposit and
                 particular invesment plans terms. Interests and returns are
-                credited to the Investor's account balance daily Monday to
+                credited to the {`Investor's`} account balance daily Monday to
                 Sunday or lumpsum at maturity, depending on investment plan
                 chosen.
               </li>
@@ -103,29 +105,29 @@ const Terms = () => {
                 The principal amount of deposit is locked in during the whole
                 investment period and can only be withdrawn after the term
                 completion unless included into daily payments. It is credited
-                to the Participant's account balance at the date of maturity,
-                and is available for disposal anytime thereafter.
+                to the {`Participant's`} account balance at the date of
+                maturity, and is available for disposal anytime thereafter.
               </li>
               <li style={{ margin: "15px" }}>
-                The funds available at the Participant's account balance bear no
-                interest and are available for withdrawal, or can be reinvested
-                again at the Participant's discretion.
+                The funds available at the {`Participant's`} account balance
+                bear no interest and are available for withdrawal, or can be
+                reinvested again at the {`Participant's`} discretion.
               </li>
             </ul>
             <h5 className={styles.termsListHeader}>Withdrawals</h5>
             <ul className={styles.termsList}>
               <li style={{ margin: "15px" }}>
                 Withdrawals are processed everyday of the week from Monday to
-                Sunday directly to the Participant's e-currency account within
-                24 hours after withdrawal request has been submitted.
+                Sunday directly to the {`Participant's`} e-currency account
+                within 24 hours after withdrawal request has been submitted.
                 Withdrawals is fast and automatic.
               </li>
               <li style={{ margin: "15px" }}>
                 All interest payments and principal withdrawals shall be paid
-                tax- free at source. The Participant understands and agrees that
+                tax-free at source. The Participant understands and agrees that
                 it is his sole responsibility to comply with any laws or
                 regulations regarding the establishment of an account or any
-                interest and withdrawal paid thereon in the Participant's
+                interest and withdrawal paid thereon in the {`Participant's`}
                 jurisdiction.
               </li>
             </ul>
@@ -135,7 +137,7 @@ const Terms = () => {
                 The Firm strictly prohibits sending unsolicited mass e-mails of
                 any kind. Use of spam will not be tolerated and the Firm will
                 terminate an account of any Participant who is caught at sending
-                unsolicited advertising messages that harm the Firm's
+                unsolicited advertising messages that harm the {`Firm's`}
                 reputation.
               </li>
               <li style={{ margin: "15px" }}>
@@ -166,14 +168,14 @@ const Terms = () => {
                 The Firm will not be liable for any losses arising from failure
                 of the Participant to hold his computer systems free from
                 malicious software used by third parties to get unauthorized
-                access to Participant's account. It is Participant's sole
-                responsibility to check his system for trojans, key loggers and
-                other malware.
+                access to {`Participant's`} account. It is {`Participant's`}{" "}
+                sole responsibility to check his system for trojans, key loggers
+                and other malware.
               </li>
               <li style={{ margin: "15px" }}>
                 The Participant agrees to hold the Firm harmless of any
-                liability regarding possible loss of Participant's funds during
-                investment process. Participant understands that every
+                liability regarding possible loss of {`Participant's`} funds
+                during investment process. Participant understands that every
                 investment he makes is been monitored and safe by the company
                 experts you have nothing to worry about your funds as we take
                 full responsibility of it we guarantee you safe investment
@@ -183,8 +185,8 @@ const Terms = () => {
           </div>
           <p className={styles.agreeTerms}>
             By creating an account with Monnay Finance Limited, you agree to be
-            bound by the Terms and Conditions above. If you don't agree, please,
-            do not go any further.
+            bound by the Terms and Conditions above. If you {`don't`} agree,
+            please, do not go any further.
           </p>
           <Link href="/signup">
             <button type="button" className={styles.termsButton}>
